5b2edcd6949a9fa761300b0fbc20c025af9591af
LeoBardo <45073941+LeoBardineo@users.noreply.github.com>
2022-08-10 23:07:24 -0300

    feat: aviãozinho ✈

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,19 +7,52 @@ public class PlayerController : MonoBehaviour
     [Header("Física")]
     public Rigidbody playerRB;
     // public GameObject groundObject;
-    public int speed = 0, jumpForce = 0, maxJumps = 1;
+    public int horizontalSpeed = 5, aviaoSpeed = 10, jumpForce = 0, maxJumps = 1;
     public float gravityScale = 5f;
     private int jumpCounter = 0;
     public bool grounded = false;
+
+    public bool isAviao = false, transformando = false;
+    private Animator animator;
+
+    private IEnumerator Metamorfose()
+    {
+        if (!transformando)
+        {
+            transformando = true;
+            isAviao = !isAviao;
+            animator.SetBool("transformando", true);            
+            animator.SetBool("isAviao", isAviao);
+            float animationLength = animator.GetCurrentAnimatorStateInfo(0).length;
+            yield return new WaitForSecondsRealtime(animationLength);
+            transformando = false;
+            animator.SetBool("transformando", false);
+        }
+    }
     private void Movement()
     {
-        playerRB.velocity = new Vector3(speed, playerRB.velocity.y, playerRB.velocity.z);
-        if ((grounded || jumpCounter < maxJumps) && Input.GetKeyDown(KeyCode.Space))
+        if (!transformando)
         {
-            jumpCounter++;
-            Vector3 upForce = Vector3.up * jumpForce;
-            Debug.Log(upForce);
-            playerRB.AddForce(upForce, ForceMode.Impulse);
+            if (isAviao)
+            {
+                playerRB.velocity = new Vector3(aviaoSpeed, playerRB.velocity.y, playerRB.velocity.z);
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Vector3 upForce = Vector3.up * jumpForce;
+                    playerRB.AddForce(upForce, ForceMode.Impulse);
+                }
+                animator.SetFloat("verticalVelocity", playerRB.velocity.y);
+            }
+            else
+            {
+                playerRB.velocity = new Vector3(horizontalSpeed, playerRB.velocity.y, playerRB.velocity.z);
+                if ((grounded || jumpCounter < maxJumps) && Input.GetKeyDown(KeyCode.Space))
+                {
+                    Vector3 upForce = Vector3.up * jumpForce;
+                    playerRB.AddForce(upForce, ForceMode.Impulse);
+                    jumpCounter++;
+                }
+            }
         }
     }
 
@@ -33,9 +66,18 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground")) { grounded = false; }
     }
 
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(Metamorfose());
+        }
         Movement();
     }
 }
