dee7ec39cb660470f419b1d20245b6e4a5d5a587
LeoBardo <45073941+LeoBardineo@users.noreply.github.com>
2022-08-11 23:16:47 -0300

    finalizei

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject Tela1, Tela2;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -13,5 +14,11 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void NextTela()
+    {
+        Tela1.SetActive(false);
+        Tela2.SetActive(true);
     }
 }
